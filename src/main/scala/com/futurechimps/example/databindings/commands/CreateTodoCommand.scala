7ca0eae51eaf3b41405da6a56786c153dd892bd1
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-11-03 23:32:16 +0000

    Docs cleanup.

--- src/main/scala/com/futurechimps/example/databindings/commands/CreateTodoCommand.scala
+++ src/main/scala/com/futurechimps/example/databindings/commands/CreateTodoCommand.scala
@@ -3,7 +3,7 @@ package com.futurechimps.example.databindings.commands
 import org.scalatra.databinding._
 import com.futurechimps.example.databindings.models._
 
-/** A command to create Todo objects. Includes validation */
+/** A command to validate and create Todo objects. */
 class CreateTodoCommand extends TodosCommand[Todo] { 
   val name: Field[String] = asType[String]("name").notBlank.minLength(3) 
 }
