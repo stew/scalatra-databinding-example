e7829664e6bed26829e3799b0dcdcefa44633e7f
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-11-08 21:39:06 +0000

    Ditching CommandHandler

--- src/main/scala/org/scalatra/example/databinding/commands/TodoCommands.scala
+++ src/main/scala/org/scalatra/example/databinding/commands/TodoCommands.scala
@@ -3,6 +3,10 @@ package org.scalatra.example.databinding.commands
 import org.scalatra.databinding._
 import org.scalatra.example.databinding.models._
 
+
+// A bit of type wizardry to make the compiler happy.
+abstract class TodosCommand[S](implicit mf: Manifest[S]) extends ModelCommand[S] with ParamsOnlyCommand
+
 /** A command to validate and create Todo objects. */
 class CreateTodoCommand extends TodosCommand[Todo] { 
   val name: Field[String] = asType[String]("name").notBlank.minLength(3) 
