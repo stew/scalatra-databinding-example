9ed64615545f57ccd215dceb7a36335298a35874
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-11-08 21:46:19 +0000

    A better explanation for the TodosCommand[S]

--- src/main/scala/org/scalatra/example/databinding/commands/TodoCommands.scala
+++ src/main/scala/org/scalatra/example/databinding/commands/TodoCommands.scala
@@ -4,7 +4,10 @@ import org.scalatra.databinding._
 import org.scalatra.example.databinding.models._
 
 
-// A bit of type wizardry to make the compiler happy.
+/** Set up an abstract class to inherit from, so we don't need to keep on
+ *  repeating the `extends ModelCommand[T] with ParamsOnlyCommand` in 
+ *  every command we make.
+ */
 abstract class TodosCommand[S](implicit mf: Manifest[S]) extends ModelCommand[S] with ParamsOnlyCommand
 
 /** A command to validate and create Todo objects. */
