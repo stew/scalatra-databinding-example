9b2c44933aab0b75681b1a389eda497080a40007
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-11-09 10:17:53 +0000

    Docs on errorFail method.

--- src/main/scala/org/scalatra/example/databinding/data/TodoData.scala
+++ src/main/scala/org/scalatra/example/databinding/data/TodoData.scala
@@ -71,5 +71,6 @@ object TodoData extends Logging with CommandHandler {
     }
   }
 
+  /** Throw a validation error if something explodes when adding a Todo **/
   def errorFail(ex: Throwable) = ValidationError(ex.getMessage, UnknownError).failNel
 }
