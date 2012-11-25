debf2face8050880d3382f63b00dabac42947044
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-11-25 20:34:45 +0000

    So this breaks everything.

--- src/main/scala/org/scalatra/example/databinding/commands/TodoCommands.scala
+++ src/main/scala/org/scalatra/example/databinding/commands/TodoCommands.scala
@@ -21,6 +21,6 @@ class CreateTodoCommand extends TodosCommand[Todo] {
   // add json format handling so the command can do automatic conversions.
   protected implicit val jsonFormats = DefaultFormats
 
-  val name: Field[String] = asType[String]("name").notBlank.minLength(3) 
+  val name: Field[String] = asType[String]("name").notBlank.minLength(3).startsWithCap
 
 }
